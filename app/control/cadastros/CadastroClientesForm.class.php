a0d817ebefaf9849055daf92827b0aeb586c652e
pedrohenrykes <pedrohenrykes@gmail.com>
2017-05-06 22:50:59 -0300

    Ajustes nas telas de cadastro de clientes

--- app/control/cadastros/CadastroClientesForm.class.php
+++ app/control/cadastros/CadastroClientesForm.class.php
@@ -1,12 +1,8 @@
 <?php
 /*
  * @author Pedro Henrique
- * @date 22/12/2016
+ * @date 06/05/2017
  */
-
-//ini_set ( 'display_errors', 1 );
-//ini_set ( 'display_startup_erros', 1 );
-//error_reporting ( E_ALL );
 
 class CadastroClientesForm extends TPage
 {
@@ -17,37 +13,28 @@ class CadastroClientesForm extends TPage
         parent::__construct();
 
         //Criacao do formulario
-        $this->form = new BootstrapFormBuilder( 'form_cadastro_clientes' );
-        $this->form->setFormTitle( 'Cadastro de Clientes' );
-        $this->form->class = 'tform';
+        $this->form = new BootstrapFormBuilder( "form_cadastro_clientes" );
+        $this->form->setFormTitle( "Cadastro de Clientes" );
+        $this->form->class = "tform";
 
         //Criacao dos campos do fomulario
-        $id               = new THidden( 'id' );
-        $nome             = new TEntry( 'nome' );
-        $cpf              = new TEntry( 'cpf' );
-        $rg               = new TEntry( 'rg' );
-        $genero           = new TCombo('genero');
-        $nascimento       = new TDate( 'nascimento' );
-        $rua              = new TEntry( 'rua' );
-        $numero           = new TEntry( 'numero' );
-        $bairro           = new TEntry( 'bairro' );
-        $cidade           = new TEntry( 'cidade' );
-        $email            = new TEntry( 'email' );
-        $dataalteracao    = new THidden( 'dataalteracao' );
-        $usuarioalteracao = new THidden( 'usuarioalteracao' );
-
-        //Componentes e variaveis adicionais do formulario
-        $label01 = new TLabel( 'Informações Pessoais', '#7D78B6', 12, 'bi' );
-        $label02 = new TLabel( 'Informações Residênciais', '#7D78B6', 12, 'bi' );
-        $label03 = new TLabel( 'Informações Contatais', '#7D78B6', 12, 'bi' );
-
-        $label01->style = 'text-align:left;border-bottom:1px solid #c0c0c0;width:100%';
-        $label02->style = 'text-align:left;border-bottom:1px solid #c0c0c0;width:100%';
-        $label03->style = 'text-align:left;border-bottom:1px solid #c0c0c0;width:100%';
+        $id               = new THidden( "id" );
+        $nome             = new TEntry( "nome" );
+        $cpf              = new TEntry( "cpf" );
+        $rg               = new TEntry( "rg" );
+        $genero           = new TCombo("genero");
+        $nascimento       = new TDate( "nascimento" );
+        $rua              = new TEntry( "rua" );
+        $numero           = new TEntry( "numero" );
+        $bairro           = new TEntry( "bairro" );
+        $cidade           = new TEntry( "cidade" );
+        $email            = new TEntry( "email" );
+        $dataalteracao    = new THidden( "dataalteracao" );
+        $usuarioalteracao = new THidden( "usuarioalteracao" );
 
         //Definicao das mascaras dos campos especiais
-        $cpf->setMask( '000.000.000-00' );
-        $nascimento->setMask( 'dd/mm/yyyy' );
+        $cpf->setMask( "000.000.000-00" );
+        $nascimento->setMask( "dd/mm/yyyy" );
 
         //Definicao de tipo de caixa das letras
         $nome->forceUpperCase();
@@ -56,52 +43,53 @@ class CadastroClientesForm extends TPage
         $cidade->forceUpperCase();
 
         //Definicao de propriedades dos campos
-        $nome->setProperty( 'title', 'O campo é obrigatório' );
-        $cpf->setProperty( 'title', 'O campos é obrigatório' );
+        $genero->setDefaultOption( "..::SELECIONE::.." );
+        $nome->setProperty( "title", "O campo é obrigatório" );
+        $cpf->setProperty( "title", "O campos é obrigatório" );
 
-        $email->setProperty( 'value', 'example@email.com' );
+        $email->setProperty( "value", "example@email.com" );
 
         //Definicao dos tamanhos de alguns campos do formulario
-        $nome->setSize( '100%' );
-        $cpf->setSize( '38%' );
-        $rg->setSize( '38%' );
-        $nascimento->setSize( '100%' );
-        $genero->setSize( '38%' );
-        $rua->setSize( '100%' );
-        $bairro->setSize( '100%' );
-        $cidade->setSize( '100%' );
+        $nome->setSize( "38%" );
+        $cpf->setSize( "38%" );
+        $rg->setSize( "38%" );
+        $nascimento->setSize( "38%" );
+        $genero->setSize( "38%" );
+        $rua->setSize( "38%" );
+        $bairro->setSize( "38%" );
+        $cidade->setSize( "38%" );
 
         //Definicao das opções dos combos
-        $genero->addItems( [ 'M' => 'Masculino', 'F' => 'Feminino' ] );
+        $genero->addItems( [ "M" => "Masculino", "F" => "Feminino" ] );
 
         //Definicao de campos obrigatorios e requeridos especiais
-        $nome->addValidation( 'Nome', new TRequiredValidator );
-        $cpf->addValidation( 'CPF', new TCPFValidator );
-        $email->addValidation( 'E-mail', new TEmailValidator );
+        $nome->addValidation( "Nome", new TRequiredValidator );
+        $cpf->addValidation( "CPF", new TRequiredValidator );
+        $email->addValidation( "E-mail", new TEmailValidator );
 
 
         //Insercao dos campos na aba de informacoes pessoais do formulario
-        $this->form->addFields( [ new TLabel( 'Nome:', '#FF0000' ) ], [ $nome ] );
-        $this->form->addFields( [ new TLabel( 'CPF:', '#FF0000' ) ], [ $cpf ]);
-        $this->form->addFields( [ new TLabel( 'RG:' ) ], [ $rg ]);
-        $this->form->addFields( [ new TLabel( 'Nascimento:' ) ], [ $nascimento ] );
-        $this->form->addFields( [ new TLabel( 'Genêro:') ], [ $genero ] );
-        $this->form->addFields( [ new TLabel( 'Rua:' ) ], [ $rua ] );
-        $this->form->addFields( [ new TLabel( 'Bairro:' ) ], [ $bairro ] );
-        $this->form->addFields( [ new TLabel( 'Cidade:' ) ], [ $cidade ] );
-        $this->form->addFields( [ new TLabel( 'E-mail:', '#FF0000' ) ], [ $email ] );
-        $this->form->addFields( [ new TLabel( 'WhatsApp:' ) ], [ '<div style="color:#FF0000">Em Breve!</div>' ] );
+        $this->form->addFields( [ new TLabel( "Nome:", "#FF0000" ) ], [ $nome ] );
+        $this->form->addFields( [ new TLabel( "CPF:", "#FF0000" ) ], [ $cpf ]);
+        $this->form->addFields( [ new TLabel( "RG:" ) ], [ $rg ]);
+        $this->form->addFields( [ new TLabel( "Nascimento:" ) ], [ $nascimento ] );
+        $this->form->addFields( [ new TLabel( "Genêro:") ], [ $genero ] );
+        $this->form->addFields( [ new TLabel( "Rua:" ) ], [ $rua ] );
+        $this->form->addFields( [ new TLabel( "Bairro:" ) ], [ $bairro ] );
+        $this->form->addFields( [ new TLabel( "Cidade:" ) ], [ $cidade ] );
+        $this->form->addFields( [ new TLabel( "E-mail:", "#FF0000" ) ], [ $email ] );
+        $this->form->addFields( [ new TLabel( "WhatsApp:" ) ], [ '<div style="color:#FF0000">Em Breve!</div>' ] );
 
         $this->form->addFields( [ $id, $usuarioalteracao, $dataalteracao ] );
 
         //Criacao dos botoes com sua determinada acoes no fomulario
-        $this->form->addAction( 'Salvar', new TAction( [ $this, 'onSave' ] ), 'fa:floppy-o' );
-        $this->form->addAction( 'Voltar para a listagem', new TAction( [ 'CadastroClientesList', 'onReload' ] ), 'fa:table blue' );
+        $this->form->addAction( "Salvar", new TAction( [ $this, "onSave" ] ), "fa:floppy-o" );
+        $this->form->addAction( "Voltar para a listagem", new TAction( [ "CadastroClientesList", "onReload" ] ), "fa:table blue" );
 
         //Criacao do container que recebe o formulario
         $container = new TVBox();
-        $container->style = 'width: 90%';
-        $container->add( new TXMLBreadCrumb( 'menu.xml', 'CadastroClientesList' ) );
+        $container->style = "width: 90%";
+        $container->add( new TXMLBreadCrumb( "menu.xml", "CadastroClientesList" ) );
         $container->add( $this->form );
 
         parent::add( $container );
@@ -114,34 +102,34 @@ class CadastroClientesForm extends TPage
             //Validacao do formulario
             $this->form->validate();
 
-            TTransaction::open( 'db_compras' );
+            TTransaction::open( "db_compras" );
 
             //Resgata os dados inseridos no formulario a partir do modelo
-            $object = $this->form->getData( 'ClientesRecord' );
+            $object = $this->form->getData( "ClientesRecord" );
 
             //Remove as mascaras dos campos especiais
             $object->cpf = preg_replace( "/[^0-9]/", "", $object->cpf );
             $object->nascimento = TDate::date2us( $object->nascimento );
 
             //Resgata o usuario a data e hora da alteracao do registro
-            $object->usuarioalteracao = $_SESSION[ 'template' ][ 'login' ];
-            $object->dataalteracao = date( 'Y-m-d H:i:s' );
+            $object->usuarioalteracao = TSession::getValue("login");
+            $object->dataalteracao = date( "Y-m-d H:i:s" );
 
             $object->store();
 
             TTransaction::close();
 
-            $action = new TAction( [ 'CadastroClientesList', 'onReload' ] );
+            $action = new TAction( [ "CadastroClientesList", "onReload" ] );
 
-            new TMessage( 'info', 'Registro salvo com sucesso!', $action );
+            new TMessage( "info", "Registro salvo com sucesso!", $action );
 
-            //TApplication::gotoPage('CadastroClientesList', 'onReload');
+            // TApplication::gotoPage("CadastroClientesList", "onReload");
         }
         catch ( Exception $ex )
         {
             TTransaction::rollback();
 
-            new TMessage( 'error', 'Ocorreu um erro ao tentar salvar o registro!<br><br>' . $ex->getMessage() );
+            new TMessage( "error", "Ocorreu um erro ao tentar salvar o registro!<br><br>" . $ex->getMessage() );
         }
     }
 
@@ -149,11 +137,11 @@ class CadastroClientesForm extends TPage
     {
         try
         {
-            if( isset( $param[ 'key' ] ) )
+            if( isset( $param[ "key" ] ) )
             {
-                TTransaction::open( 'db_compras' );
+                TTransaction::open( "db_compras" );
 
-                $object = new ClientesModel( $param[ 'key' ] );
+                $object = new ClientesRecord( $param[ "key" ] );
 
                 $object->nascimento = TDate::date2br( $object->nascimento );
 
@@ -166,7 +154,7 @@ class CadastroClientesForm extends TPage
         {
             TTransaction::rollback();
 
-            new TMessage( 'error', 'Ocorreu um erro ao tentar carregar o registro para edição!<br><br>' . $ex->getMessage() );
+            new TMessage( "error", "Ocorreu um erro ao tentar carregar o registro para edição!<br><br>" . $ex->getMessage() );
         }
     }
 }
--- app/control/cadastros/CadastroClientesList.class.php
+++ app/control/cadastros/CadastroClientesList.class.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * @author Pedro Henrique
+ * @date 06/05/2017
+ */
 
 class CadastroClientesList extends TPage
 {
@@ -21,35 +25,36 @@ class CadastroClientesList extends TPage
         $dados = new TEntry( "dados" );
 
         // Definicao de propriedades dos campos
-        $opcao->setDefaultOption( '..::SELECIONE::..' );
-        $dados->setProperty( 'title', 'Informe os dados de acordo com a opção' );
+        $opcao->setDefaultOption( "..::SELECIONE::.." );
+        $dados->setProperty( "title", "Informe os dados de acordo com a opção" );
+        $dados->forceUpperCase();
 
         // Definicao dos tamanhos do campos
-        $opcao->setSize( '38%' );
-        $dados->setSize( '38%' );
+        $opcao->setSize( "38%" );
+        $dados->setSize( "38%" );
 
         // Definicao das opções dos combos
-        $opcao->addItems( [ 'nome' => 'Nome', 'cpf' => 'CPF', 'rg' => 'RG' ] );
+        $opcao->addItems( [ "nome" => "Nome", "cpf" => "CPF", "rg" => "RG" ] );
 
-        $this->form->addFields( [ new TLabel( 'Opção de filtro:' ) ], [ $opcao ] );
-        $this->form->addFields( [ new TLabel( 'Dados da busca:' ) ], [ $dados ] );
+        $this->form->addFields( [ new TLabel( "Opção de filtro:" ) ], [ $opcao ] );
+        $this->form->addFields( [ new TLabel( "Dados da busca:" ) ], [ $dados ] );
 
         // Criacao dos botoes com sua determinada acoes no fomulario
-        $this->form->addAction( 'Buscar', new TAction( [ $this, 'onSearch' ] ), 'fa:search' );
-        $this->form->addAction( 'Novo', new TAction( [ 'CadastroClientesForm', 'onEdit' ] ), 'bs:plus-sign green' );
+        $this->form->addAction( "Buscar", new TAction( [ $this, "onSearch" ] ), "fa:search" );
+        $this->form->addAction( "Novo", new TAction( [ "CadastroClientesForm", "onEdit" ] ), "bs:plus-sign green" );
 
         //Criacao do datagrid de listagem de dados
         $this->datagrid = new BootstrapDatagridWrapper( new TDataGrid() );
-        $this->datagrid->datatable = 'true';
-        $this->datagrid->style = 'width: 100%';
+        $this->datagrid->datatable = "true";
+        $this->datagrid->style = "width: 100%";
         $this->datagrid->setHeight( 320 );
 
         //Criacao das colunas do datagrid
-        $column_id = new TDataGridColumn( 'id', 'ID', 'center', 50 );
-        $column_nome = new TDataGridColumn( 'nome', 'Nome', 'left' );
-        $column_cpf = new TDataGridColumn( 'cpf', 'CPF', 'left' );
-        $column_rg = new TDataGridColumn( 'rg', 'RG', 'left' );
-        $column_email = new TDataGridColumn( 'email', 'E-mail', 'center' );
+        $column_id = new TDataGridColumn( "id", "ID", "center", 50 );
+        $column_nome = new TDataGridColumn( "nome", "Nome", "left" );
+        $column_cpf = new TDataGridColumn( "cpf", "CPF", "left" );
+        $column_rg = new TDataGridColumn( "rg", "RG", "left" );
+        $column_email = new TDataGridColumn( "email", "E-mail", "center" );
 
         //Insercao das colunas no datagrid
         $this->datagrid->addColumn( $column_id );
@@ -59,71 +64,249 @@ class CadastroClientesList extends TPage
         $this->datagrid->addColumn( $column_email );
 
         //Insercao das acoes de ordenacao nas colunas do datagrid
-        $order_id = new TAction( [ $this, 'onReload' ] );
-        $order_id->setParameter( 'order', 'id' );
+        $order_id = new TAction( [ $this, "onReload" ] );
+        $order_id->setParameter( "order", "id" );
         $column_id->setAction( $order_id );
 
-        $order_nome = new TAction( [ $this, 'onReload' ] );
-        $order_nome->setParameter( 'order', 'nome' );
+        $order_nome = new TAction( [ $this, "onReload" ] );
+        $order_nome->setParameter( "order", "nome" );
         $column_nome->setAction( $order_nome );
 
-        $order_cpf = new TAction( [ $this, 'onReload' ] );
-        $order_cpf->setParameter( 'order', 'cpf' );
+        $order_cpf = new TAction( [ $this, "onReload" ] );
+        $order_cpf->setParameter( "order", "cpf" );
         $column_cpf->setAction( $order_cpf );
 
-        $order_rg = new TAction( [ $this, 'onReload' ] );
-        $order_rg->setParameter( 'order', 'rg' );
+        $order_rg = new TAction( [ $this, "onReload" ] );
+        $order_rg->setParameter( "order", "rg" );
         $column_rg->setAction( $order_rg );
 
-        $order_email = new TAction( [ $this, 'onReload' ] );
-        $order_email->setParameter( 'order', 'situacao' );
+        $order_email = new TAction( [ $this, "onReload" ] );
+        $order_email->setParameter( "order", "situacao" );
         $column_email->setAction( $order_email );
 
         //Criacao da acao de edicao no datagrid
-        $action_edit = new TDataGridAction( [ 'CadastroClientesForm', 'onEdit' ] );
-        $action_edit->setButtonClass( 'btn btn-default' );
-        $action_edit->setLabel( 'Editar' );
-        $action_edit->setImage( 'fa:pencil-square-o blue fa-lg' );
-        $action_edit->setField( 'id' );
+        $action_edit = new TDataGridAction( [ "CadastroClientesForm", "onEdit" ] );
+        $action_edit->setButtonClass( "btn btn-default" );
+        $action_edit->setLabel( "Editar" );
+        $action_edit->setImage( "fa:pencil-square-o blue fa-lg" );
+        $action_edit->setField( "id" );
         $this->datagrid->addAction( $action_edit );
 
         //Criacao da acao de delecao no datagrid
-        $action_del = new TDataGridAction( [ $this, 'onDelete' ] );
-        $action_del->setButtonClass( 'btn btn-default' );
-        $action_del->setLabel( 'Deletar' );
-        $action_del->setImage( 'fa:trash-o red fa-lg' );
-        $action_del->setField( 'id' );
+        $action_del = new TDataGridAction( [ $this, "onDelete" ] );
+        $action_del->setButtonClass( "btn btn-default" );
+        $action_del->setLabel( "Deletar" );
+        $action_del->setImage( "fa:trash-o red fa-lg" );
+        $action_del->setField( "id" );
         $this->datagrid->addAction( $action_del );
 
         //Exibicao do datagrid
         $this->datagrid->createModel();
+
+        //Criacao do navedor de paginas do datagrid
+        $this->pageNavigation = new TPageNavigation();
+        $this->pageNavigation->setAction( new TAction( [ $this, "onReload" ] ) );
+        $this->pageNavigation->setWidth( $this->datagrid->getWidth() );
+
         // Criacao do container que recebe o formulario
         $container = new TVBox();
         $container->style = "width: 90%";
+        $container->add( new TXMLBreadCrumb( "menu.xml", __CLASS__ ) );
         $container->add( $this->form );
         $container->add( TPanelGroup::pack( NULL, $this->datagrid ) );
+        $container->add( $this->pageNavigation );
 
         // Adicionando o container com o form a pagina
         parent::add( $container );
     }
 
+    public function onReload( $param = NULL )
+    {
+        try
+        {
+            // Abrindo a conexao com o banco de dados
+            TTransaction::open( "db_compras" );
+
+            // Criando um repositorio para armazenar temporariamente os dados do banco
+            $repository = new TRepository( "ClientesRecord" );
+
+            if ( empty( $param[ "order" ] ) )
+            {
+                $param[ "order" ] = "id";
+                $param[ "direction" ] = "asc";
+            }
+
+            $limit = 10;
+
+            // Criando um criterio de busca no banco de dados
+            $criteria = new TCriteria();
+            $criteria->setProperties( $param );
+            $criteria->setProperty( "limit", $limit );
+
+            // Buscando os dados no banco de acordo com os criterios passados
+            $objects = $repository->load( $criteria, FALSE );
+
+            // Limpando o datagrid
+            $this->datagrid->clear();
+
+            // Se existirem dados no banco, o datagrid sera prenchido por esse foreach
+            if ( !empty( $objects ) )
+            {
+                foreach ( $objects as $object )
+                {
+                    $this->datagrid->addItem( $object );
+                }
+            }
+
+            $criteria->resetProperties();
+
+            // Salvando a contagem dos registros que estam no repositorio
+            $count = $repository->count($criteria);
+
+            $this->pageNavigation->setCount($count); // Definindo quantos registros tera por pagina do datagrid
+            $this->pageNavigation->setProperties($param); // Definindo os paramentros de organizacao dos dados por pagina
+            $this->pageNavigation->setLimit($limit); // Definindo o limite de registros por pagina do datagrid
+
+            // Fechando a conexao com o banco de dados
+            TTransaction::close();
+
+            $this->loaded = true;
+        }
+        catch ( Exception $ex )
+        {
+            TTransaction::rollback();
+
+            new TMessage( "error", $ex->getMessage() );
+        }
+    }
+
     public function onSearch()
     {
+        $data = $this->form->getData();
+
+        try
+        {
+            if( !empty( $data->opcao ) && !empty( $data->dados ) )
+            {
+                TTransaction::open( "db_compras" );
+
+                $repository = new TRepository( "ClientesRecord" );
+
+                if ( empty( $param[ "order" ] ) )
+                {
+                    $param[ "order" ] = "id";
+                    $param[ "direction" ] = "asc";
+                }
+
+                $limit = 10;
+
+                $criteria = new TCriteria();
+                $criteria->setProperties( $param );
+                $criteria->setProperty( "limit", $limit );
+
+                if( $data->opcao == "nome" )
+                {
+                    $criteria->add( new TFilter( $data->opcao, "LIKE", "%" . $data->dados . "%" ) );
+                }
+                else if ( ( $data->opcao == "cpf" || $data->opcao == "rg" ) && ( is_numeric( $data->dados ) ) )
+                {
+                    $criteria->add( new TFilter( $data->opcao, "LIKE", $data->dados . "%" ) );
+                }
+                else
+                {
+                    new TMessage( "error", "O valor informado não é valido para um " . strtoupper( $data->opcao ) . "." );
+                }
+
+                $objects = $repository->load( $criteria, FALSE );
+
+                $this->datagrid->clear();
+
+                if ( $objects )
+                {
+                    foreach ( $objects as $object )
+                    {
+                        $this->datagrid->addItem( $object );
+                    }
+                }
+
+                $criteria->resetProperties();
+
+                $count = $repository->count( $criteria );
+
+                $this->pageNavigation->setCount( $count ); // count of records
+                $this->pageNavigation->setProperties( $param ); // order, page
+                $this->pageNavigation->setLimit( $limit ); //Limita a quantidade de registros
+
+                TTransaction::close();
+
+                $this->form->setData( $data );
+
+                $this->loaded = true;
+
+            }
+            else
+            {
+                $this->onReload();
+
+                $this->form->setData( $data );
+
+                new TMessage( "error", "Selecione uma opção e informe os dados da busca corretamente!" );
+            }
+        }
+        catch ( Exception $ex )
+        {
+            TTransaction::rollback();
+
+            $this->form->setData( $data );
+
+            new TMessage( "error", $ex->getMessage() );
+        }
 
     }
 
-    public function onReload()
+    public function onDelete( $param = NULL )
     {
+        if( isset( $param[ "key" ] ) )
+        {
+            //Criacao das acoes a serem executadas na mensagem de exclusao
+            $action1 = new TAction( [ $this, "Delete" ] );
+            $action2 = new TAction( [ $this, "onReload" ] );
 
+            //Definicao sos parametros de cada acao
+            $action1->setParameter( "key", $param[ "key" ] );
+
+            new TQuestion( "Deseja realmente apagar o registro?", $action1, $action2 );
+        }
     }
 
-    public function onDelete()
+    function Delete( $param = NULL )
     {
+        try
+        {
+            TTransaction::open( "db_compras" );
 
+            $object = new ClientesRecord( $param[ "key" ] );
+
+            $object->delete();
+
+            TTransaction::close();
+
+            $this->onReload();
+
+            new TMessage("info", "Registro apagado com sucesso!");
+        }
+        catch ( Exception $ex )
+        {
+            TTransaction::rollback();
+
+            new TMessage("error", $ex->getMessage());
+        }
     }
 
-    public function onTurnOnOff()
+    public function show()
     {
+        $this->onReload();
 
+        parent::show();
     }
 }
