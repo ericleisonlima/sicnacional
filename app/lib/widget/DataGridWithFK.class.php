7a95617b0a268af24be87642053d605f0874a19c
pedrohenrykes <pedrohenrykes@gmail.com>
2017-05-18 11:48:09 -0300

    Criação da classe para colocar um FK no datagrid action

---
+++ app/lib/widget/DataGridWithFK.class.php
@@ -0,0 +1,16 @@
+<?php
+
+class DataGridWithFK extends TDataGrid
+{
+    public function __construct()
+    {
+       parent::__construct();
+
+
+    }
+
+    public function setFK()
+    {
+
+    }
+}
