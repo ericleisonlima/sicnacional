008545f98aa336fa8591910f4da40c124db39602
SAPE99\admin <geronimocpd@gmail.com>
2017-05-18 11:48:01 -0300

    Alteração de estabelecimento Form para detalhe

--- app/control/cadastros/EstabelecimentoList.class.php
+++ app/control/cadastros/EstabelecimentoList.class.php
@@ -1,8 +1,5 @@
 <?php
-
-
-class EstabelecimentoList extends TPage
-{
+class EstabelecimentoList extends TPage{
     private $form;
     private $datagrid;
     private $pageNavigation;
@@ -12,7 +9,6 @@ class EstabelecimentoList extends TPage
     {
         parent::__construct();
 
-        // Criacao do formulario
         $this->form = new BootstrapFormBuilder( "form_list_cadastro_estabelecimento" );
         $this->form->setFormTitle( "Listagem de Estabelecimento" );
         $this->form->class = "tform";
@@ -20,66 +16,38 @@ class EstabelecimentoList extends TPage
         $opcao = new TCombo( "opcao" );
         $dados = new TEntry( "dados" );
 
-        // Definicao de propriedades dos campos
         $opcao->setDefaultOption( "..::SELECIONE::.." );
         $dados->setProperty( "title", "Informe os dados de acordo com a opção" );
         $dados->forceUpperCase();
 
-        // Definicao dos tamanhos do campos
-        $opcao->setSize( "38%" );
-        $dados->setSize( "38%" );
-
-        // Definicao das opções dos combos
         $opcao->addItems( [  "nome" => "Nome"] );
 
         $this->form->addFields( [ new TLabel( "Opção de filtro:" ) ], [ $opcao ] );
         $this->form->addFields( [ new TLabel( "Dados da busca:" ) ], [ $dados ] );
 
-        // Criacao dos botoes com sua determinada acoes no fomulario
         $this->form->addAction( "Buscar", new TAction( [ $this, "onSearch" ] ), "fa:search" );
         $this->form->addAction( "Novo", new TAction( [ "EstabelecimentoForm", "onEdit" ] ), "bs:plus-sign green" );
 
-        //Criacao do datagrid de listagem de dados
         $this->datagrid = new BootstrapDatagridWrapper( new TDataGrid() );
         $this->datagrid->datatable = "true";
         $this->datagrid->style = "width: 100%";
         $this->datagrid->setHeight( 320 );
 
-        //Criacao das colunas do datagrid
-       // $column_id = new TDataGridColumn( "id", "ID", "center", 50 );
         $column_nome = new TDataGridColumn( "nome", "Nome", "left" );
         $column_cpf = new TDataGridColumn( "endereco", "Endereço", "left" );
         $column_rg = new TDataGridColumn( "bairro", "Bairro", "left" );
         $column_email = new TDataGridColumn( "cep", "Cep", "center" );
 
-        //Insercao das colunas no datagrid
-       // $this->datagrid->addColumn( $column_id );
         $this->datagrid->addColumn( $column_nome );
         $this->datagrid->addColumn( $column_cpf );
         $this->datagrid->addColumn( $column_rg );
         $this->datagrid->addColumn( $column_email );
 
-        //Insercao das acoes de ordenacao nas colunas do datagrid
-        /*
-        $order_id = new TAction( [ $this, "onReload" ] );
-        $order_id->setParameter( "order", "id" );
-        $column_id->setAction( $order_id );
-        */
 
         $order_nome = new TAction( [ $this, "onReload" ] );
         $order_nome->setParameter( "order", "nome" );
         $column_nome->setAction( $order_nome );
 
-
-        //Criacao da acao de edicao no datagrid
-        $action_edit = new TDataGridAction( [ "EstabelecimentoForm", "onEdit" ] );
-        $action_edit->setButtonClass( "btn btn-default" );
-        $action_edit->setLabel( "Editar" );
-        $action_edit->setImage( "fa:pencil-square-o blue fa-lg" );
-        $action_edit->setField( "id" );
-        $this->datagrid->addAction( $action_edit );
-
-        //Criacao da acao de delecao no datagrid
         $action_del = new TDataGridAction( [ $this, "onDelete" ] );
         $action_del->setButtonClass( "btn btn-default" );
         $action_del->setLabel( "Deletar" );
@@ -87,100 +55,86 @@ class EstabelecimentoList extends TPage
         $action_del->setField( "id" );
         $this->datagrid->addAction( $action_del );
 
-        //Exibicao do datagrid
+        $action_edit = new TDataGridAction( [ "EstabelecimentoForm", "onEdit" ] );
+        $action_edit->setButtonClass( "btn btn-default" );
+        $action_edit->setLabel( "Editar" );
+        $action_edit->setImage( "fa:pencil-square-o blue fa-lg" );
+        $action_edit->setField( "id" );
+        $this->datagrid->addAction( $action_edit );
+
+        $action_estab_med = new TDataGridAction( [ "EstabelecimentoMedicoDetalhe", "onReload" ] );
+        $action_estab_med->setButtonClass( "btn btn-default" );
+        $action_estab_med->setLabel( "Editar" );
+        $action_estab_med->setImage( "fa:pencil-square-o blue fa-lg" );
+        $action_estab_med->setField( "id" );
+        $action_estab_med->setFk( "id" );
+        $this->datagrid->addAction( $action_estab_med );
+
         $this->datagrid->createModel();
 
-        //Criacao do navedor de paginas do datagrid
         $this->pageNavigation = new TPageNavigation();
         $this->pageNavigation->setAction( new TAction( [ $this, "onReload" ] ) );
         $this->pageNavigation->setWidth( $this->datagrid->getWidth() );
 
-        // Criacao do container que recebe o formulario
         $container = new TVBox();
         $container->style = "width: 90%";
-       // $container->add( new TXMLBreadCrumb( "menu.xml", __CLASS__ ) );
         $container->add( $this->form );
         $container->add( TPanelGroup::pack( NULL, $this->datagrid ) );
         $container->add( $this->pageNavigation );
 
-        // Adicionando o container com o form a pagina
         parent::add( $container );
     }
 
-    public function onReload( $param = NULL )
-    {
-        try
-        {
-            // Abrindo a conexao com o banco de dados
+    public function onReload( $param = NULL ){
+        try{
             TTransaction::open( "dbsic" );
 
-            // Criando um repositorio para armazenar temporariamente os dados do banco
             $repository = new TRepository( "EstabelecimentoRecord" );
 
-            if ( empty( $param[ "order" ] ) )
-            {
+            if ( empty( $param[ "order" ] ) ){
                 $param[ "order" ] = "id";
                 $param[ "direction" ] = "asc";
             }
 
             $limit = 10;
 
-            // Criando um criterio de busca no banco de dados
             $criteria = new TCriteria();
             $criteria->setProperties( $param );
             $criteria->setProperty( "limit", $limit );
 
-            // Buscando os dados no banco de acordo com os criterios passados
             $objects = $repository->load( $criteria, FALSE );
-
-            // Limpando o datagrid
             $this->datagrid->clear();
-
-            // Se existirem dados no banco, o datagrid sera prenchido por esse foreach
-            if ( !empty( $objects ) )
-            {
-                foreach ( $objects as $object )
-                {
+            if ( !empty( $objects ) ){
+                foreach ( $objects as $object ){
                     $this->datagrid->addItem( $object );
                 }
             }
 
             $criteria->resetProperties();
-
-            // Salvando a contagem dos registros que estam no repositorio
             $count = $repository->count($criteria);
 
-            $this->pageNavigation->setCount($count); // Definindo quantos registros tera por pagina do datagrid
-            $this->pageNavigation->setProperties($param); // Definindo os paramentros de organizacao dos dados por pagina
-            $this->pageNavigation->setLimit($limit); // Definindo o limite de registros por pagina do datagrid
-
-            // Fechando a conexao com o banco de dados
+            $this->pageNavigation->setCount($count); 
+            $this->pageNavigation->setProperties($param);
+            $this->pageNavigation->setLimit($limit);
             TTransaction::close();
 
             $this->loaded = true;
-        }
-        catch ( Exception $ex )
-        {
+        }catch ( Exception $ex ){
             TTransaction::rollback();
 
             new TMessage( "error", $ex->getMessage() );
         }
     }
 
-    public function onSearch()
-    {
+    public function onSearch(){
       $data = $this->form->getData();
 
-        try
-        {
-            if( !empty( $data->opcao ) && !empty( $data->dados ) )
-            {
+        try{
+            if( !empty( $data->opcao ) && !empty( $data->dados ) ){
                 TTransaction::open( "dbsic" );
 
                 $repository = new TRepository( "EstabelecimentoRecord" );
-
-                if ( empty( $param[ "order" ] ) )
-                {
+                if ( empty( $param[ "order" ] ) ){
                     $param[ "order" ] = "id";
                     $param[ "direction" ] = "asc";
                 }
@@ -191,16 +145,14 @@ class EstabelecimentoList extends TPage
                 $criteria->setProperties( $param );
                 $criteria->setProperty( "limit", $limit );
 
-                if( $data->opcao == "nome" )
-                {
+                if( $data->opcao == "nome" ){
                     $criteria->add( new TFilter( $data->opcao, "LIKE", "%" . $data->dados . "%" ) );
                 }
-                else if ( is_numeric($data->dados))
-                {
+
+                else if ( is_numeric($data->dados)){
                     $criteria->add( new TFilter($data->opcao,'=',$data->dados) );
-                }
-                else
-                {
+
+                }else{
                     new TMessage( "error", "O valor informado não é valido para um " . strtoupper( $data->opcao ) . "." );
                 }
 
@@ -208,95 +160,67 @@ class EstabelecimentoList extends TPage
 
                 $this->datagrid->clear();
 
-                if ( $objects )
-                {
-                    foreach ( $objects as $object )
-                    {
+                if ( $objects ){
+                    foreach ( $objects as $object ){
                         $this->datagrid->addItem( $object );
                     }
                 }
 
                 $criteria->resetProperties();
-
                 $count = $repository->count( $criteria );
 
-                $this->pageNavigation->setCount( $count ); // count of records
-                $this->pageNavigation->setProperties( $param ); // order, page
-                $this->pageNavigation->setLimit( $limit ); //Limita a quantidade de registros
+                $this->pageNavigation->setCount( $count );
+                $this->pageNavigation->setProperties( $param );
+                $this->pageNavigation->setLimit( $limit );
 
                 TTransaction::close();
-
                 $this->form->setData( $data );
-
                 $this->loaded = true;
 
-            }
-            else
-            {
+            }else{
                 $this->onReload();
-
                 $this->form->setData( $data );
 
                 new TMessage( "error", "Selecione uma opção e informe os dados da busca corretamente!" );
             }
-        }
-        catch ( Exception $ex )
-        {
+        }catch ( Exception $ex ){
             TTransaction::rollback();
-
             $this->form->setData( $data );
-
             new TMessage( "error", $ex->getMessage() );
         }
 
 
     }
 
-    public function onDelete( $param = NULL )
-    {
+    public function onDelete( $param = NULL ){
 
-        if( isset( $param[ "key" ] ) )
-        {
-            //Criacao das acoes a serem executadas na mensagem de exclusao
+        if( isset( $param[ "key" ] ) ){
             $action1 = new TAction( [ $this, "Delete" ] );
             $action2 = new TAction( [ $this, "onReload" ] );
-
-            //Definicao sos parametros de cada acao
             $action1->setParameter( "key", $param[ "key" ] );
 
             new TQuestion( "Deseja realmente apagar o registro?", $action1, $action2 );
         }
     }
 
-    function Delete( $param = NULL )
-    {
+    function Delete( $param = NULL ){
 
-        try
-        {
+        try{
             TTransaction::open( "dbsic" );
-
             $object = new EstabelecimentoRecord( $param[ "key" ] );
-
             $object->delete();
-
             TTransaction::close();
-
             $this->onReload();
 
             new TMessage("info", "Registro apagado com sucesso!");
-        }
-        catch ( Exception $ex )
-        {
+        }catch ( Exception $ex ){
             TTransaction::rollback();
-
             new TMessage("error", $ex->getMessage());
         }
     }
 
-    public function show()
-    {
+    public function show(){
         $this->onReload();
-
         parent::show();
     }
 }
--- app/control/cadastros/detalhes/EstabelecimentoMedicoDetalhe.class.php
+++ app/control/cadastros/detalhes/EstabelecimentoMedicoDetalhe.class.php
@@ -1,5 +1,5 @@
 <?php
-class EstabelecimentoMedicoForm extends TPage
+class EstabelecimentoMedicoDetalhe extends TPage
 {
     private $form;
     public function __construct()
