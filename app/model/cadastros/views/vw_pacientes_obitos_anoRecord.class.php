163e0431b674b5b4367e3a602a55ed3ad2f6968c
GABRIEL DE PAULA CORDEIRO <inadequado@unp.edu.br>
2017-09-06 18:04:55 -0300

    criado grafico para pacientes em obito

---
+++ app/model/cadastros/views/vw_pacientes_obitos_anoRecord.class.php
@@ -0,0 +1,11 @@
+<?php
+
+class vw_pacientes_obitos_anoRecord extends TRecord
+{
+    const TABLENAME = 'vw_pacientes_obitos';
+    const PRIMARYKEY= 'id';
+    const IDPOLICY =  'max'; // {max, serial}
+    
+
+
+}
