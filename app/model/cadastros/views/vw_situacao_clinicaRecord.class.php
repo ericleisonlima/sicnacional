d9f4ed7613321f166b7a2f69ba8b26927a461ebf
ERICLEISON LIMA DE CARVALHO <ericleison@unp.edu.br>
2017-09-06 15:39:19 -0300

    Grafico de situação clinica

---
+++ app/model/cadastros/views/vw_situacao_clinicaRecord.class.php
@@ -0,0 +1,10 @@
+<?php
+
+class vw_situacao_clinicaRecord extends TRecord
+{
+    const TABLENAME = 'vw_situacao_clinica';
+    const PRIMARYKEY= 'id';
+    const IDPOLICY =  'max'; // {max, serial}
+    
+}
+?>
