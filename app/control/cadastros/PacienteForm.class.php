1e1a2692d2b6e8f7ced8f05f42ea0579f66a4025
inadequado <gpcordeiro@hotmail.com>
2018-06-08 11:49:49 -0300

    Mudança tela de paciente

--- app/control/cadastros/PacienteForm.class.php
+++ app/control/cadastros/PacienteForm.class.php
@@ -16,55 +16,69 @@ class PacienteForm extends TPage
         $this->form->setFormTitle( "Cadastro de Paciente" );
         $this->form->class = "tform";
 
-        $this->form->setNotebookWrapper( new BootstrapNotebookWrapper(new TNotebook) );
+        $notebook = new BootstrapNotebookWrapper(new TNotebook);
+        $this->form->add($notebook);
+
+        $page1 = new TTable;
+        $page2 = new TTable;
+        $page1->style = 'padding: 10px';
+        $page2->style = 'padding: 10px';
+        
+        // adds two pages in the notebook
+        $notebook->appendPage('Informações Pessoais', $page1);
+        $notebook->appendPage('Informações Clinicas', $page2);
 
         $id = new THidden( "id" );
 
         ##--Informações Pessoais--#
 
         $nome                   = new TEntry( "nome" );
-        $municipio_id           = new TCombo( "municipio_id" );
-        $nascimento             = new TDate( "datanascimento" );        
         $telefone               = new TEntry( "telefone" );
         $email                  = new TEntry( "email" );
         $nmoradores             = new TEntry("nmoradores");
-        $etnia                  = new TEntry("etnia"); 
         $profissao              = new TEntry("profissao");
         $naturalidade           = new TEntry("naturalidade");
-        $residencia             = new TEntry("residencia");
-        $estado_civil           = new TEntry("estado_civil");        
-        $peso_habitual          = new TEntry("peso_habitual");
-        $altura                 = new TEntry("altura");        
-        $data_inicio_fumante    = new TDate("data_inicio_fumante");
-        $data_fim_fumante       = new TDate("data_fim_fumante");        
-        $data_inicio_alcoolista = new TDate("data_inicio_alcoolista");
-        $data_fim_alcoolista    = new TDate("data_fim_alcoolista");
-        $tipo_fumante           = new TRadioGroup("tipo_fumante");
-        $tipo_alcoolista        = new TRadioGroup("tipo_alcoolista");
+        $residencia             = new TEntry("residencia");    
+        $procedencia            = new TEntry("procedencia"); 
+        $municipio_id           = new TCombo( "municipio_id" );  
+        $etnia                  = new TCombo("etnia"); 
+        $estado_civil           = new TCombo("estado_civil");  
+        $nascimento             = new TDate( "datanascimento" );             
         $empregado              = new TRadioGroup ("empregado");
 
         ##--Informações Clinicas--##
 
-        $qtd_cirurgia              = new TEntry("qtd_cirurgia");
-        $descricao_cirurgia        = new TEntry("descricao_cirurgia");
-        $tiposanguineo             = new TCombo( "tiposanguineo" ); 
-        $fatorsanguineo            = new TCombo( "fatorsanguineo" );
-        $condicoes_diagnostico_id  = new TCombo("condicoes_diagnostico_id");
-        $estabelecimento_medico_id = new TCombo( "estabelecimento_medico_id" );
-        $situacao_clinica_id       = new TCombo( "situacao_clinica_id" );
-        $datadiagnostico           = new TDate( "datadiagnostico" );
-        $sexo                      = new TRadioGroup('sexo');
+        $qtd_cirurgia               = new TEntry("qtd_cirurgia");
+        $peso_habitual              = new TEntry("peso_habitual");
+        $altura                 = new TEntry("altura"); 
+        $descricao_cirurgia         = new TText("descricao_cirurgia");
+        $tiposanguineo              = new TCombo( "tiposanguineo" ); 
+        $fatorsanguineo             = new TCombo( "fatorsanguineo" );
+        $condicoes_diagnostico_id   = new TCombo("condicoes_diagnostico_id");
+        $estabelecimento_medico_id  = new TCombo( "estabelecimento_medico_id" );
+        $situacao_clinica_id        = new TCombo( "situacao_clinica_id" );
+        $datadiagnostico            = new TDate( "datadiagnostico" );
+        $data_inicio_fumante        = new TDate("data_inicio_fumante");
+        $data_fim_fumante           = new TDate("data_fim_fumante");        
+        $data_inicio_alcoolista     = new TDate("data_inicio_alcoolista");
+        $data_fim_alcoolista        = new TDate("data_fim_alcoolista");
+        $tipo_fumante               = new TRadioGroup("tipo_fumante");
+        $tipo_alcoolista            = new TRadioGroup("tipo_alcoolista");
+        $sexo                       = new TRadioGroup('sexo');
 
         ##--Radios Groups--##
 
         $sexo->addItems(array('M'=>'Masculino', 'F'=>'Feminino'));
         $sexo->setLayout('horizontal');
 
-        $tipo_fumante->addItems(array('S'=>'Sim', 'N'=>'Não'));
-        $tipo_fumante->setLayout('horizontal');          
+        $tipo_fumante->addItems(array('SIM'=>'Sim', 'NAO'=>'Não','EX-ALCOOLISTA'=>'Ex-Fumante'));
+        $tipo_fumante->setLayout('horizontal'); 
+        $tipo_fumante->setValue('NAO');
 
-        $tipo_alcoolista->addItems(array('S'=>'Sim', 'N'=>'Não'));
+
+        $tipo_alcoolista->addItems(array('SIM'=>'Sim', 'NAO'=>'Não', 'EX-ALCOOLISTA'=>'Ex-Alcoolista'));
         $tipo_alcoolista->setLayout('horizontal'); 
+        $tipo_alcoolista->setValue('NAO');
 
         $empregado->addItems(array('S'=>'Sim', 'N'=>'Não'));
         $empregado->setLayout('horizontal'); 
@@ -85,6 +99,11 @@ class PacienteForm extends TPage
 
         $fatorsanguineo->addItems( [ "P" => "Positivo", "N" => "Negativo" ] );
 
+        $estado_civil->addItems( [ "SOLTEIRO" => "Solteiro", "CASADO" => "Casado", "VIUVO" => "Viuvo", 
+            "UNIAO_ESTAVEL" => "União Estavel", "DIVORCIADO"=>"Divorciado", "OUTROS"=>"Outros" ] );    
+        
+        $etnia->addItems( [ "BRANCO" => "Branco", "PARDO" => "Pardo", "NEGRO" => "Negro", "AMARELO" => "Amarelo", "INDIGENA" => "Indígena" ] ); 
+
         $condicoes_diagnostico_id = new TDBCombo('condicoes_diagnostico_id', 'dbsic', 'CondicoesDiagnosticoRecord', 
             'id', 'descricao', 'descricao');
 
@@ -95,8 +114,8 @@ class PacienteForm extends TPage
         $municipio_id = new TDBCombo('municipio_id', 'dbsic', 'MunicipioRecord', 'id', 'nome', 'nome');
         $municipio_id->style = "text-transform: uppercase;";
         $municipio_id->setProperty('placeholder', '....::::DIGITE O MUNICÍPIO::::....');
-        $municipio_id->setMinLength(3);
-        $municipio_id->setSize('30%');
+        //$municipio_id->setMinLength('3');
+        $municipio_id->setSize('100%');
         $municipio_id->enableSearch();      
 
       
@@ -128,41 +147,80 @@ class PacienteForm extends TPage
         $estabelecimento_medico_id->addValidation( "Estabelecimento Médico", new TRequiredValidator );
         $situacao_clinica_id->addValidation( "Situação Clinica", new TRequiredValidator );
 
-        ##--Campos--##
-         $this->form->appendPage('Page 1');
-         $this->form->addFields( [ new TLabel( "Nome:<font color=red><b>*</b></font> ") ], [ $nome ] );
-         $this->form->addFields( [ new TLabel( "Nascimento:<font color=red>*</font>" ) ], [ $nascimento ] );
-         $this->form->addFields( [ new TLabel( "Sexo:" ) ], [ $sexo ] );
-         $this->form->addFields( [ new TLabel( "Município:<font color=red><b>*</b></font>" ) ], [ $municipio_id ]);
-         $this->form->addFields( [ new TLabel( "Data Diagnóstico:<font color=red>*</font>" ) ], [ $datadiagnostico ] );
-         $this->form->addFields( [ new TLabel( "Condições Diagnóstico:<font color=red>* ") ], 
-            [ $condicoes_diagnostico_id ] );
-         $this->form->addFields( [ new TLabel( "Estabelecimento Médico:<font color=red><b>*</b></font>") ], [ $estabelecimento_medico_id ] );
-         $this->form->addFields( [ new TLabel( "Situação Clinica:<font color=red><b>*</b></font>") ], [ $situacao_clinica_id ] );
-         $this->form->addFields( [ new TLabel( "Tipo Sanguíneo:<font color=red>*</font>") ], [ $tiposanguineo ] );
-         $this->form->addFields( [ new TLabel( "Fator Sanguíneo:<font color=red>*</font>" ) ], [ $fatorsanguineo ] );
-
-         $this->form->addFields( [ new TLabel( "E-Mail:" ) ], [ $email ] );
-         $this->form->addFields( [ new TLabel( "Telefone:" ) ], [ $telefone ] );
-
-        $this->form->appendPage('Page 2');
-        $this->form->addFields( [ new TLabel( "Cirurgias Realizadas:" ) ], [ $qtd_cirurgia ] );
-        $this->form->addFields( [ new TLabel( "Descrição das Cirurgias:" ) ], [ $descricao_cirurgia ] );
-        $this->form->addFields( [ new TLabel( "Fumante:" ) ], [ $tipo_fumante ] );
-        $this->form->addFields( [ new TLabel( "Alcoolista:" ) ], [ $tipo_alcoolista ] );
-        $this->form->addFields( [ new TLabel( "Trabalha:" ) ], [ $empregado ] );
-        $this->form->addFields( [ new TLabel( "Profissão:" ) ], [ $profissao ] );
-
-
+        ##--Campos Informações Pessoais--##
         
-        //$this->form->addFields( [ new TLabel( "Causa Óbito:") ], [ $causa_obito ]);
-        //$this->form->addFields( [ new TLabel( "Data Óbito:" ) ], [ $dataobito ] );
+        $page1->addRowSet( [ new TLabel( "Nome:<font color=red><b>*</b></font> ") ], [ $nome ] );
+        $page1->addRowSet( [ new TLabel( "Nascimento:<font color=red>*</font>" ) ], [ $nascimento ] );
+        $page1->addRowSet( [ new TLabel( "Sexo:" ) ], [ $sexo ] );
+        $page1->addRowSet( [ new TLabel( "Etnia:" ) ], [ $etnia ] );
+        $page1->addRowSet( [ new TLabel( "Naturalidade:" ) ], [ $naturalidade ] );
+        $page1->addRowSet( [ new TLabel( "Procedencia:" ) ], [ $procedencia ] );
+        $page1->addRowSet( [ new TLabel( "Residencia:" ) ], [ $residencia ] );
+        $page1->addRowSet( [ new TLabel( "Número de Moradores:" ) ], [ $nmoradores ] );
+        $page1->addRowSet( [ new TLabel( "Estado Civil:" ) ], [ $estado_civil ] );
 
-        $this->form->addField( new TLabel('<font color=red><b>* Campos Obrigatórios </b></font>');
+        $page1->addRowSet( [ new TLabel( "Município:<font color=red><b>*</b></font>" ) ], [ $municipio_id ]);
+        $page1->addRowSet( [ new TLabel( "Trabalha:" ) ], [ $empregado ] );
+        $page1->addRowSet( [ new TLabel( "Profissão:" ) ], [ $profissao ] );        
+        $page1->addRowSet( [ new TLabel( "E-Mail:" ) ], [ $email ] );
+        $page1->addRowSet( [ new TLabel( "Telefone:" ) ], [ $telefone ] );
+        
+
+        ##--Campos Informações Clinicas--##
+        $page2->addRowSet( [ new TLabel( "Data Diagnóstico:<font color=red>*</font>" ) ], [ $datadiagnostico ] );
+        $page2->addRowSet( [ new TLabel( "Estabelecimento Médico:<font color=red><b>*</b></font>") ], 
+            [ $estabelecimento_medico_id ] );
+        $page2->addRowSet( [ new TLabel( "Situação Clinica:<font color=red><b>*</b></font>") ], 
+            [ $situacao_clinica_id ] );
+        $page2->addRowSet( [ new TLabel( "Condições Diagnóstico:<font color=red>* ") ], 
+            [ $condicoes_diagnostico_id ] );
+        $page2->addRowSet( [ new TLabel( "Peso Habitual:" ) ], [ $peso_habitual ] );
+        $page2->addRowSet( [ new TLabel( "Cirurgias Realizadas:" ) ], [ $qtd_cirurgia ] );
+        $page2->addRowSet( [ new TLabel( "Descrição das Cirurgias:" ) ], [ $descricao_cirurgia ] ); 
+        $page2->addRowSet( [ new TLabel( "Fumante:" ) ], [ $tipo_fumante ] );
+        $page2->addRowSet( [ new TLabel( "Inicio do Tabagismo:" ) ], [ $data_inicio_fumante ] );
+        $page2->addRowSet( [ new TLabel( "Fim do Tabagismo:" ) ], [ $data_fim_fumante ] );
+        $page2->addRowSet( [ new TLabel( "Alcoolista:" ) ], [ $tipo_alcoolista ] );
+        $page2->addRowSet( [ new TLabel( "Inicio Alcoolista:" ) ], [ $data_inicio_alcoolista ] );
+        $page2->addRowSet( [ new TLabel( "Fim Alcoolista:" ) ], [ $data_fim_alcoolista ] );
+        $page2->addRowSet( [ new TLabel( "Altura" ) ], [ $altura ] );   
+        $page2->addRowSet( [ new TLabel( "Tipo Sanguíneo:<font color=red>*</font>") ], [ $tiposanguineo ] );
+        $page2->addRowSet( [ new TLabel( "Fator Sanguíneo:<font color=red>*</font>" ) ], [ $fatorsanguineo ] );
+           
+
+
+        ##--Set Fields--##
+
+        $this->form->setFields(array(
+            $nome, $nascimento, $sexo, $municipio_id, $datadiagnostico,
+            $estabelecimento_medico_id, $situacao_clinica_id, 
+            $tiposanguineo, $fatorsanguineo, $email, $telefone, $qtd_cirurgia, 
+            $descricao_cirurgia, $tipo_fumante, $tipo_alcoolista, $empregado, $profissao, $data_inicio_fumante,
+            $data_fim_fumante, $data_inicio_alcoolista, $data_fim_alcoolista, $etnia, $estado_civil, $residencia, 
+            $procedencia, $nmoradores, $naturalidade, $altura
+             ));
+
+        ##--Buttões--##
+        
+        $this->form->addFields( [new TLabel('<font color=red><b>* Campos Obrigatórios </b></font>'), []] );
         $this->form->addFields( [ $id ] );
 
         $this->form->addAction( "Salvar", new TAction( [ $this, "onSave" ] ), "fa:floppy-o" );
-        $this->form->addAction( "Voltar para a listagem", new TAction( [ "PacienteList", "onReload" ] ), "fa:table blue" );
+        $this->form->addAction( "Voltar para a listagem", new TAction( [ "PacienteList", "onReload" ] ),
+         "fa:table blue" );
+
+        ##--Change Action--##
+
+            $acaofumante = new TAction(array($this, 'onChangeFumante'));
+            $acaofumante->setParameter('formName', $this->form->getName());
+            $tipo_fumante->setChangeAction($acaofumante);
+
+            $acaoalcoolista = new TAction(array($this, 'onChangeAlcoolista'));
+            $acaoalcoolista->setParameter('formName', $this->form->getName());
+            $tipo_alcoolista->setChangeAction($acaoalcoolista);
+
+
+        ##--container--##
       
         $container = new TVBox();
         $container->style = "width: 90%";
@@ -213,6 +271,72 @@ class PacienteForm extends TPage
         {
             TTransaction::rollback();
             new TMessage( "error", "Ocorreu um erro ao tentar carregar o registro para edição!<br><br>" . $ex->getMessage() );
+        }
+    }
+
+    public static function onChangeFumante($param)
+    {
+        switch ($param['tipo_fumante'])
+        {
+            case 'SIM':
+            
+            TCombo::clearField($param['formName'], 'data_fim_fumante');
+            TCombo::disableField($param['formName'], 'data_fim_fumante');
+            
+            TCombo::enableField($param['formName'], 'data_inicio_fumante');
+            
+            break;
+        
+            case 'NAO':
+            
+            TCombo::clearField($param['formName'], 'data_fim_fumante');
+            TCombo::disableField($param['formName'], 'data_inicio_fumante');
+
+            TCombo::clearField($param['formName'], 'data_fim_fumante');
+            TCombo::disableField($param['formName'], 'data_inicio_fumante'); 
+             
+            break;
+
+            case 'EX-FUMANTE':
+            
+            TCombo::enableField($param['formName'], 'data_inicio_fumante');    
+            TCombo::enableField($param['formName'], 'data_fim_fumante');    
+ 
+            break;
+
+        }
+    }
+
+    public static function onChangeAlcoolista($param)
+    {
+        switch ($param['tipo_alcoolista'])
+        {
+            case 'SIM':
+            
+            TCombo::clearField($param['formName'], 'data_fim_alcoolista');
+            TCombo::disableField($param['formName'], 'data_fim_alcoolista');
+            
+            TCombo::enableField($param['formName'], 'data_inicio_alcoolista');
+            
+            break;
+        
+            case 'NAO':
+            
+            TCombo::clearField($param['formName'], 'data_fim_alcoolista');
+            TCombo::disableField($param['formName'], 'data_inicio_alcoolista');
+
+            TCombo::clearField($param['formName'], 'data_fim_alcoolista');
+            TCombo::disableField($param['formName'], 'data_inicio_alcoolista'); 
+             
+            break;
+
+            case 'EX-ALCOOLISTA':
+            
+            TCombo::enableField($param['formName'], 'data_inicio_alcoolista');    
+            TCombo::enableField($param['formName'], 'data_fim_alcoolista');    
+ 
+            break;
+
         }
     }
 
